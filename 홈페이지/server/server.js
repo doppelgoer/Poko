2dccc3595526aa09335ca51337c5b7a26306f570
SeaweedOne <rlakskchlrh@naver.com>
2021-10-24 13:33:36 +0900

    pull hana

--- 홈페이지/server/server.js
+++ 홈페이지/server/server.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const api = require('./routes/index');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
+const api = require("./routes/index");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api', api);
-let mysql = require('mysql');
-const router = require('./routes/index');
+app.use("/api", api);
+let mysql = require("mysql");
+const router = require("./routes/index");
 let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'poko', // 데이터베이스 고르기
-  port: '3307',
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "poko", // 데이터베이스 고르기
+  port: "3306",
 });
 
 connection.connect();
@@ -26,12 +26,12 @@ const port = process.env.PORT || 80;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.use(express.static(path.join(__dirname, '../build')));
-app.get('/', function (req, res) {
-  res.send(express.static(path.join(__dirname, '../build/index.html')));
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("/", function (req, res) {
+  res.send(express.static(path.join(__dirname, "../build/index.html")));
 });
 
-app.get('/text', async function (req, res) {
+app.get("/text", async function (req, res) {
   console.log(3213213, req.query);
   let selectTestSql = `SELECT * FROM test`;
   let selectTestRes = await query(selectTestSql);
@@ -39,7 +39,7 @@ app.get('/text', async function (req, res) {
   res.send(selectTestRes[0]);
 });
 
-app.get('/getBoardContents', async function (req, res) {
+app.get("/getBoardContents", async function (req, res) {
   //   console.log(123123);
   //   console.log(req.query);
   let getBoardContentsSql = `SELECT idx,title FROM board_content`;
@@ -62,8 +62,7 @@ query = function (q) {
 };
 
 // Contact US page
-app.post('/contactUs', async function (req, res) {
+app.post("/contactUs", async function (req, res) {
   console.log(1234);
   console.log(3213213, req.body);
-
 });
