9808d4a5187fea121a6996b9ffcb1c448639d5c7
unknown <doppelgoer1@naver.com>
2021-12-28 17:18:33 +0900

    홈페이지 모든 페이지 boxing완료

--- 홈페이지/src/components/teamPage/TeamPage.js
+++ 홈페이지/src/components/teamPage/TeamPage.js
@@ -3,9 +3,14 @@ import TeamCard from "./TeamCard";
 import "../../css/contactUs.css";
 import Title from "../publicCp/Title";
 import axios from "axios";
-import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 const headAddress = `http://localhost`;
+
 function Team() {
+  const isTablet = useMediaQuery({
+    query: "(max-width:1024px)",
+  });
+  // console.log(isTablet);
   // const [textbox, setTextBox] = useState([]);
   // const [userName, setUserName] = useState("");
   // const [userEmail, setUserEmail] = useState("");
@@ -36,30 +41,53 @@ function Team() {
   // useEffect(() => {
   //     insertInfo();
   // }, []);
-
-  return (
-    <div className="teamPage">
-      <div className="teamPageContainer">
-        <Title title="OUR MEMBER"></Title>
-        <div className="memberContainer">
-          <TeamCard></TeamCard>
-          <TeamCard></TeamCard>
-          <TeamCard></TeamCard>
-          <TeamCard></TeamCard>
-          <TeamCard txt="POKO"></TeamCard>
-          <TeamCard></TeamCard>
-          <TeamCard></TeamCard>
-          <TeamCard></TeamCard>
+  if (isTablet) {
+    return (
+      <div className="teamPage">
+        <div className="teamPageContainer">
+          <Title title="OUR MEMBER"></Title>
+          <div className="memberContainer">
+            <TeamCard txt="WE ARE POKO"></TeamCard>
+            <TeamCard txt="김정연"></TeamCard>
+            <TeamCard txt="김하나"></TeamCard>
+            <TeamCard txt="김보라"></TeamCard>
+            <TeamCard txt="장준영"></TeamCard>
+            <TeamCard txt="간략한"></TeamCard>
+          </div>
         </div>
-      </div>
 
-      {/* <div
+        {/* <div
         style={{ backgroundColor: "white", width: "100vw", height: "100vh" }}
       >
         Our Team
       </div> */}
-    </div>
-  );
+      </div>
+    );
+  } else if (!isTablet) {
+    return (
+      <div className="teamPage">
+        <div className="teamPageContainer">
+          <Title title="OUR MEMBER"></Title>
+          <div className="memberContainer">
+            <TeamCard txt="김정연"></TeamCard>
+            <TeamCard txt="김하나"></TeamCard>
+            <TeamCard txt="여기는"></TeamCard>
+            <TeamCard txt="김보라"></TeamCard>
+            <TeamCard txt="POKO"></TeamCard>
+            <TeamCard txt="장준영"></TeamCard>
+            <TeamCard txt="간략한"></TeamCard>
+            <TeamCard txt="문자가 들어감"></TeamCard>
+          </div>
+        </div>
+
+        {/* <div
+        style={{ backgroundColor: "white", width: "100vw", height: "100vh" }}
+      >
+        Our Team
+      </div> */}
+      </div>
+    );
+  }
 }
 
 export default Team;
