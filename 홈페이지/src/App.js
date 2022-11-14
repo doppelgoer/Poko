58a6526e79f194e0426727b926d1c9e1ad6ae41e
doppelgoer <doppelgoer1@naver.com>
2022-11-14 11:32:28 +0900

    push

--- 홈페이지/src/App.js
+++ 홈페이지/src/App.js
@@ -5,10 +5,19 @@ import Header from "./components/header/Header";
 import Pro from "./components/productsPage/Product";
 import Team from "./components/teamPage/TeamPage";
 import LoginPage from "./components/LoginPage";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import axios from "axios";
 function App() {
+  async function postUserInfo() {
+    let addData = await axios({
+      method: "post",
+      url: `https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400`,
+      data: {
+        // data 는 post,   params은 get방식
+      },
+    });
+  }
   return (
     <div className="background">
       <div className="App">
@@ -16,7 +25,7 @@ function App() {
           <Header />
           <Switch>
             <Route path="/" exact component={Main}></Route>
-            {/* <Route path="/Board" component={Board}></Route> */}
+            <Route path="/Board" component={Board}></Route>
             {/* <Route path="/products" component={Products}></Route> */}
             <Route path="/products" component={Pro}></Route>
             <Route path="/Contact" component={Team}></Route>
@@ -24,6 +33,7 @@ function App() {
           </Switch>
         </Router>
       </div>
+      <button onClick={postUserInfo}>dddddddddddd</button>
     </div>
   );
 }
